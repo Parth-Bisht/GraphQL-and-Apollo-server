118cb9c95b856f8d69d81165f99dd958d840b858
Parth Bisht <bishtparth795@gmail.com>
2023-05-05 12:14:21 +0530

    apollo client

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+import { GET_ALL_QUOTES } from "../gqloperations/queries";
 
 const Home = () => {
+  const {loading,error,data} = useQuery(GET_ALL_QUOTES);
+  // useEffect(() => {
+  //   fetch("http://localhost:4000", {
+  //     method: "post",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       query: `
+  //       query getAllQuotes{
+  //         quotes{
+  //           name
+  //           by{
+  //             _id
+  //             first_name
+  //           }
+  //         }
+  //       }
+  //       `,
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
   return (
     <div className="container">
       <blockquote>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <div class="nav-wrapper #1565c0 blue darken-3">
-        <Link to="/" class="brand-logo left">
+      <div className="nav-wrapper #1565c0 blue darken-3">
+        <Link to="/" className="brand-logo left">
           Make Quotes
         </Link>
-        <ul id="nav-mobile" class="right">
+        <ul id="nav-mobile" className="right">
           <li>
             <Link to="/login">Login</Link>
           </li>
